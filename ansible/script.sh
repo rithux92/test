b91932a4a2970b00a3b71cb4fbbe435dc10067ce
Ubuntu <ubuntu@ip-172-31-31-162.ap-south-1.compute.internal>
2019-03-14 14:59:05 +0000

    adding ansible dir to h1 branch

---
+++ ansible/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pwd >> /home/ubuntu/script_file.txt
+echo 'first arg: $1' >> /home/ubuntu/script_file.txt
+echo '===============================' >> /home/ubuntu/script_file.txt
+date >> /home/ubuntu/script_file.txt
+ifconfig >> /home/ubuntu/script_file.txt
+
